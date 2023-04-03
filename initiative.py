d8f51cc174870583aa894e4ac2f05cb3bc843504
Malicious <104476480+dglassow@users.noreply.github.com>
2023-04-02 18:16:43 -0700

    Create initiative.py
    A simple script to roll for initiative and organize combat.

---
+++ initiative.py
@@ -0,0 +1,25 @@
+#/*******************************************************************************\
+#|                                                                               |
+#| - Created by D.Glassow & J. Weathers                                          |
+#|                                                                               |
+#| - any variable or function inside will be available from any of your scripts. |
+#|                                                                               |
+#\*******************************************************************************/
+
+# take user input for party size
+# take user input for monstar party size
+
+# Prompt user: Roll for party? Y/N
+  #If No, take user imput for party values
+# Roll for monsters
+# Save all values to an array & sort by turn
+
+
+
+#/*******************************************************************************\
+#|                                                                               |
+#| End sciprt                                                                    |
+#|                                                                               |
+#\*******************************************************************************/
+# TO DO
+# Include monster stats and execute combat rolls

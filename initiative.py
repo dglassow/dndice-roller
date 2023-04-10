cf9116e7be8acd38c8738c1a152a96321c2bc7fa
PistolPete821 <128565499+PistolPete821@users.noreply.github.com>
2023-04-09 19:16:19 -0700

    Update initiative.py

--- initiative.py
+++ initiative.py
@@ -6,11 +6,33 @@
 #|                                                                               |
 #\*******************************************************************************/
 
-# take user input for party size
-# take user input for monster party size
 
+# take user input for party size
+pSize = input("Enter party size: ")
+print('Party size is: ' + pSize)
+
+# take user input for monster party size
+mSize = (input("Enter monster party size: "))
+print('Monster size is: ' + mSize)
 # Prompt user: Roll for party? Y/N
   #If No, take user imput for party values
+pMembers = []
+
+while True:
+ roll = input("roll for party? Y/N: ")
+ if roll.upper() == "Y":
+     print()
+     print("Rolling for party")
+     print()
+     exit()
+
+ elif roll.upper() == "N":
+     print("__________________________")
+     roll = input("Please enter party order ")
+     exit()
+ else:
+     continue
+
 # Roll for monsters
 # Save all values to an array & sort by turn
 

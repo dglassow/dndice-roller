057aaa4f975dc20fbc9fea10b2cdf255766900f1
PistolPete821 <128565499+PistolPete821@users.noreply.github.com>
2023-04-10 19:16:03 -0700

    Player initiative
    Player initiative working

--- initiative.py
+++ initiative.py
@@ -5,40 +5,50 @@
 #| - any variable or function inside will be available from any of your scripts. |
 #|                                                                               |
 #\*******************************************************************************/
-
+import random
 
 # take user input for party size
 pSize = input("Enter party size: ")
-print('Party size is: ' + pSize)
+pSize = int(pSize)
 
 # take user input for monster party size
 mSize = (input("Enter monster party size: "))
-print('Monster size is: ' + mSize)
+
+#Prompt user: Enter players
+pMembers = []
+initiative = []
+players = {'Name': [pMembers], 'roll': [initiative]}
+
+for i in range(pSize):
+  pName = input("Enter player name: ")
+  pMembers.append(pName)
+
+
 # Prompt user: Roll for party? Y/N
   #If No, take user imput for party values
-pMembers = []
-
-for pMembers in pSize:
-  x = input('Enter players: ')
-  pMembers.append(x)
-
-print(pMembers)
 
 while True:
- roll = input("Roll for party? Y/N: ")
- if roll.upper() == "Y":
-     print("Rolling for party")
-     exit()
+  roll = input("Roll for party? Y/N: ")
+  if roll.upper() == "Y":
+    print("Rolling for party")
+    for i in range(pSize):
+      rolls = random.randrange(1,20)
+      initiative.append(rolls)
+    break
 
- elif roll.upper() == "N":
-     roll = input("Please enter party order ")
-     exit()
- else:
-     continue
+  elif roll.upper() == "N":
+    for i in range(pSize):
+      rolls = input("Please enter initiative for " + pMembers[i] + ':')
+      initiative.append(rolls)
+    break
+  else:
+    print('Invalid Entry')
+    break
 
+for i in range(pSize):
+  print(pMembers[i], initiative[i])
 # Roll for monsters
 # Save all values to an array & sort by turn
-
 
 
 #/*******************************************************************************\
